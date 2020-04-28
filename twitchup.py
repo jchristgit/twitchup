4ff6a6fb65a45f8edcd1fa75ac93f4f15b091f6d
Johannes Christ <jc@jchri.st>
2020-04-28 22:25:34 +0200

    Format code.

--- twitchup.py
+++ twitchup.py
@@ -20,7 +20,7 @@ reddit = praw.Reddit(
     client_secret=os.environ['REDDIT_CLIENT_SECRET'],
     username=os.environ['REDDIT_USERNAME'],
     password=os.environ['REDDIT_PASSWORD'],
-    user_agent=f"{sys.platform}:twitchup:0.3.0 (by /u/Volcyy)"
+    user_agent=f"{sys.platform}:twitchup:0.3.0 (by /u/Volcyy)",
 )
 
 logging.basicConfig(
@@ -38,7 +38,7 @@ SUBREDDIT_NAME = os.environ['SUBREDDIT_NAME']
 
 def chunks(over: List[str], size: int) -> Generator[List[str], None, None]:
     for i in range(0, len(over), size):
-        yield over[i:i + size]
+        yield over[i : i + size]
 
 
 def fetch_access_token() -> str:
@@ -63,13 +63,17 @@ def load_online(names: List[str], access_token: str):
         url=f"https://api.twitch.tv/helix/streams?first=100&{login_params}",
         headers={
             'Authorization': f'Bearer {access_token}',
-            'Client-Id': os.environ['TWITCH_CLIENT_ID']
-        }
+            'Client-Id': os.environ['TWITCH_CLIENT_ID'],
+        },
     )
     try:
         with urlopen(request) as response:
             if response.getcode() != 200:
-                raise ValueError(f"unexpected code %d, response %r", response.getcode(), response.read())
+                raise ValueError(
+                    f"unexpected code %d, response %r",
+                    response.getcode(),
+                    response.read(),
+                )
             streams = json.load(response)
             return {stream['user_name'] for stream in streams.get('data', ())}
     except HTTPError as response:
@@ -97,7 +101,9 @@ def render_template(template: str, online_streams: Set[str]) -> str:
         else:
             link_title = 'twitch-offline'
 
-        markdown_link = f"[{stream_name}](https://twitch.tv/{stream_name} '{link_title}')"
+        markdown_link = (
+            f"[{stream_name}](https://twitch.tv/{stream_name} '{link_title}')"
+        )
         invocation = f'twitchup({stream_name})'
         log.debug("Replaced %r with %r.", invocation, markdown_link)
         template = template.replace(invocation, markdown_link)
@@ -111,13 +117,13 @@ if __name__ == '__main__':
         '--log-level',
         help="Level to emit logging output at.",
         default='INFO',
-        choices=('DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL')
+        choices=('DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL'),
     )
     parser.add_argument(
         '-t',
         '--template-directory',
         help="Where to find templates.",
-        default='templates'
+        default='templates',
     )
     args = parser.parse_args()
     log.setLevel(args.log_level)
@@ -148,8 +154,7 @@ if __name__ == '__main__':
     }
     online = get_online_streams(list(names), access_token)
     log.info(
-        "Loaded stream information for %d streams, %d online.",
-        len(names), len(online)
+        "Loaded stream information for %d streams, %d online.", len(names), len(online)
     )
 
     for subreddit_name, template in sidebars.items():
@@ -162,7 +167,10 @@ if __name__ == '__main__':
             mod_relationship.update(description=sidebar)
             log.info("Updated sidebar on %r with new stream data.", subreddit_name)
         else:
-            log.info("Omitting sidebar update on %r as no changes would be done.", subreddit_name)
+            log.info(
+                "Omitting sidebar update on %r as no changes would be done.",
+                subreddit_name,
+            )
 
     for subreddit_name, template in widgets.items():
         rendered = render_template(template, online)
@@ -173,13 +181,13 @@ if __name__ == '__main__':
                         "Skipping non-stream widget %r (%s) on %r.",
                         widget.shortName,
                         widget.__class__.__name__,
-                        subreddit_name
+                        subreddit_name,
                     )
 
                 elif widget.text == rendered:
                     log.info(
                         "Omitting widget update on %r as no changes would be done.",
-                        subreddit_name
+                        subreddit_name,
                     )
 
                 else:
@@ -189,7 +197,7 @@ if __name__ == '__main__':
                         log.exception(
                             "Unable to update sidebar widget on %r:",
                             subreddit_name,
-                            exc_info=err
+                            exc_info=err,
                         )
                     else:
                         log.info("Rendered widget update on %r.", subreddit_name)
